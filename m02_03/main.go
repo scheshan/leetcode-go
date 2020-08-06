0415ddb6a13943b8c8477cd9731556a6bd0102d7
heshan <5373827@qq.com>
2020-08-06 14:52:11 +0800

    m02_03

---
+++ m02_03/main.go
@@ -0,0 +1,15 @@
+package m02_03
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteNode(node *ListNode) {
+	if node == nil || node.Next == nil {
+		return
+	}
+
+	node.Val = node.Next.Val
+	node.Next = node.Next.Next
+}
