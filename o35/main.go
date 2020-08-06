d717c3ececba367c7589f965303f8d53bae940e9
heshan <5373827@qq.com>
2020-08-06 16:13:35 +0800

    o35

---
+++ o35/main.go
@@ -0,0 +1,30 @@
+package o35
+
+type Node struct {
+	Val    int
+	Next   *Node
+	Random *Node
+}
+
+func copyRandomList(head *Node) *Node {
+	m := make(map[*Node]*Node)
+	return copy(m, head)
+}
+
+func copy(m map[*Node]*Node, node *Node) *Node {
+	if node == nil {
+		return nil
+	}
+
+	if m[node] != nil {
+		return m[node]
+	}
+
+	res := new(Node)
+	m[node] = res
+	res.Val = node.Val
+
+	res.Random = copy(m, node.Random)
+	res.Next = copy(m, node.Next)
+	return res
+}
