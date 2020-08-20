19d18c494e93fda9aaaecb620dea10fccb472b0e
heshan <5373827@qq.com>
2020-08-20 14:36:05 +0800

    o18

---
+++ o18/main.go
@@ -0,0 +1,23 @@
+package o18
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteNode(head *ListNode, val int) *ListNode {
+	dummy := new(ListNode)
+	tail := dummy
+
+	for head != nil {
+		if head.Val != val {
+			tail.Next = head
+			tail = tail.Next
+		}
+
+		head = head.Next
+	}
+
+	tail.Next = nil
+	return dummy.Next
+}
