86b61c0368039f0a28c0e6012291bb08cdcc4368
heshan <5373827@qq.com>
2023-01-10 08:00:20 +0800

    l108

---
+++ leetcode/l108/main.go
@@ -0,0 +1,21 @@
+package l108
+
+import "github.com/scheshan/leetcode/common"
+
+type TreeNode = common.TreeNode
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	return buildTree(nums, 0, len(nums)-1)
+}
+
+func buildTree(nums []int, left int, right int) *TreeNode {
+	if left > right {
+		return nil
+	}
+
+	mid := (left + right) >> 1
+	node := &TreeNode{Val: nums[mid]}
+	node.Left = buildTree(nums, left, mid-1)
+	node.Right = buildTree(nums, mid+1, right)
+	return node
+}
