93b4ca74310175190a767571fd414552dfb74336
scheshan <5373827@qq.com>
2023-01-10 14:40:25 +0800

    o55

---
+++ leetcode/o55/main.go
@@ -0,0 +1,53 @@
+package o55
+
+import (
+	"github.com/scheshan/leetcode/common"
+)
+
+type TreeNode = common.TreeNode
+
+type BSTIterator struct {
+	stack []*TreeNode
+}
+
+func Constructor(root *TreeNode) BSTIterator {
+	res := BSTIterator{
+		stack: make([]*TreeNode, 0),
+	}
+	res.push(root)
+	return res
+}
+
+func (this *BSTIterator) Next() int {
+	for {
+		node := this.pop()
+		if node == nil {
+			return this.pop().Val
+		}
+
+		if node.Right != nil {
+			this.push(node.Right)
+		}
+
+		this.push(node)
+		this.push(nil)
+
+		if node.Left != nil {
+			this.push(node.Left)
+		}
+	}
+}
+
+func (this *BSTIterator) HasNext() bool {
+	return len(this.stack) > 0
+}
+
+func (this *BSTIterator) pop() *TreeNode {
+	node := this.stack[len(this.stack)-1]
+	this.stack = this.stack[0 : len(this.stack)-1]
+	return node
+}
+
+func (this *BSTIterator) push(node *TreeNode) {
+	this.stack = append(this.stack, node)
+}
