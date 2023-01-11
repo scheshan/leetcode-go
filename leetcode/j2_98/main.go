63b790ee90634e8921f38ac4f4f752ff3bd10e0b
scheshan <5373827@qq.com>
2023-01-11 17:23:01 +0800

    j2_98

---
+++ leetcode/j2_98/main.go
@@ -0,0 +1,16 @@
+package j2_98
+
+func uniquePaths(m int, n int) int {
+	state := make([]int, n)
+	for i := 0; i < n; i++ {
+		state[i] = 1
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			state[j] = state[j] + state[j-1]
+		}
+	}
+
+	return state[n-1]
+}
