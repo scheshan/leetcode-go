e3f93f29f78fb0249750f0adfc6085fd2c5790be
heshan <5373827@qq.com>
2023-01-11 19:48:00 +0800

    j2_012

---
+++ leetcode/j2_012/main.go
@@ -0,0 +1,23 @@
+package main
+
+func pivotIndex(nums []int) int {
+	for i := 1; i < len(nums); i++ {
+		nums[i] += nums[i-1]
+	}
+
+	total := nums[len(nums)-1]
+	for i := 0; i < len(nums); i++ {
+		left := 0
+		if i > 0 {
+			left = nums[i-1]
+		}
+		if total-nums[i] == left {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+	pivotIndex([]int{1, 7, 3, 6, 5, 6})
+}
