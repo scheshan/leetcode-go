5abdcd7ce2a51517261a2bbeff6773e14c58d029
scheshan <5373827@qq.com>
2023-01-11 18:19:23 +0800

    j2_003

---
+++ leetcode/j2_003/main.go
@@ -0,0 +1,20 @@
+package j2_003
+
+func countBits(n int) []int {
+	res := make([]int, n+1)
+	if n > 0 {
+		res[1] = 1
+	}
+
+	for i := 2; i <= n; i++ {
+		pre := i >> 1
+		cnt := res[pre]
+
+		if i&1 == 1 {
+			cnt++
+		}
+		res[i] = cnt
+	}
+
+	return res
+}
