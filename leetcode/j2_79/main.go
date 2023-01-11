9301194142d95022d67c342a72875ee3db69a95c
scheshan <5373827@qq.com>
2023-01-11 16:14:10 +0800

    j2_79

---
+++ leetcode/j2_79/main.go
@@ -0,0 +1,25 @@
+package main
+
+func subsets(nums []int) [][]int {
+	path := make([]int, 0, len(nums))
+	res := make([][]int, 0)
+
+	perm(nums, -1, &path, &res)
+	return res
+}
+
+func perm(nums []int, ind int, path *[]int, res *[][]int) {
+	r := make([]int, len(*path))
+	copy(r, *path)
+	*res = append(*res, r)
+
+	for i := ind + 1; i < len(nums); i++ {
+		*path = append(*path, nums[i])
+		perm(nums, i, path, res)
+		*path = (*path)[:len(*path)-1]
+	}
+}
+
+func main() {
+	subsets([]int{1, 2, 3})
+}
