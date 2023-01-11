e9f94db7223c46e55ef555bfdd34bf9b27617547
scheshan <5373827@qq.com>
2023-01-11 16:53:46 +0800

    j2_110

--- leetcode/j2_110/main.go
+++ leetcode/j2_110/main.go
@@ -1,16 +1,18 @@
 package j2_110
 
 func allPathsSourceTarget(graph [][]int) [][]int {
-	path := make([]int, 0)
+	path := make([]int, 0, len(graph))
 	path = append(path, 0)
+	visit := make([]bool, len(graph))
+	visit[0] = true
 	res := make([][]int, 0)
 
-	dfs(graph, 0, make(map[int]bool), &path, &res)
+	dfs(graph, 0, visit, &path, &res)
 
 	return res
 }
 
-func dfs(graph [][]int, n int, visit map[int]bool, path *[]int, res *[][]int) {
+func dfs(graph [][]int, n int, visit []bool, path *[]int, res *[][]int) {
 	if n == len(graph)-1 {
 		r := make([]int, len(*path))
 		copy(r, *path)
@@ -28,7 +30,7 @@ func dfs(graph [][]int, n int, visit map[int]bool, path *[]int, res *[][]int) {
 
 		dfs(graph, next, visit, path, res)
 
-		delete(visit, next)
+		visit[next] = false
 		*path = (*path)[:len(*path)-1]
 	}
 }
