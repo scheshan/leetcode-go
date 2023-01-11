39b2f71498c0b7be94b82af2c3e3bc1116a49f2c
scheshan <5373827@qq.com>
2023-01-11 17:10:52 +0800

    j2_91

---
+++ leetcode/j2_91/main.go
@@ -0,0 +1,19 @@
+package j2_91
+
+func minCost(costs [][]int) int {
+	for i := 1; i < len(costs); i++ {
+		costs[i][0] += min(costs[i-1][1], costs[i-1][2])
+		costs[i][1] += min(costs[i-1][0], costs[i-1][2])
+		costs[i][2] += min(costs[i-1][0], costs[i-1][1])
+	}
+
+	last := len(costs) - 1
+	return min(costs[last][0], min(costs[last][1], costs[last][2]))
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
