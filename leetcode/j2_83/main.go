26788c4b16663639f85c8e7407961dc1601c6454
scheshan <5373827@qq.com>
2023-01-11 15:31:23 +0800

    j2_83

---
+++ leetcode/j2_83/main.go
@@ -0,0 +1,32 @@
+package main
+
+func permute(nums []int) [][]int {
+	path := make([]int, 0, len(nums))
+	res := make([][]int, 0)
+
+	perm(nums, make(map[int]bool), &path, &res)
+	return res
+}
+
+func perm(nums []int, visit map[int]bool, path *[]int, res *[][]int) {
+	if len(*path) == len(nums) {
+		r := make([]int, len(nums), len(nums))
+		copy(r, *path)
+		*res = append(*res, r)
+		return
+	}
+
+	for _, num := range nums {
+		if !visit[num] {
+			visit[num] = true
+			*path = append(*path, num)
+			perm(nums, visit, path, res)
+			*path = (*path)[:len(*path)-1]
+			visit[num] = false
+		}
+	}
+}
+
+func main() {
+	permute([]int{1, 2, 3})
+}
