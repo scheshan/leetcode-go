980220edbf3b2ec92380a204f5f5dff37007464a
scheshan <5373827@qq.com>
2023-01-12 13:03:08 +0800

    j2_038

---
+++ leetcode/j2_038/main.go
@@ -0,0 +1,39 @@
+package j2_038
+
+type Stack struct {
+	arr []int
+}
+
+func (t *Stack) Push(v int) {
+	t.arr = append(t.arr, v)
+}
+
+func (t *Stack) Pop() int {
+	n := t.Peek()
+	t.arr = t.arr[:len(t.arr)-1]
+	return n
+}
+
+func (t *Stack) Peek() int {
+	return t.arr[len(t.arr)-1]
+}
+
+func (t *Stack) Len() int {
+	return len(t.arr)
+}
+
+func dailyTemperatures(temperatures []int) []int {
+	stack := &Stack{arr: make([]int, 0)}
+	res := make([]int, len(temperatures))
+
+	for i, n := range temperatures {
+		for stack.Len() > 0 && temperatures[stack.Peek()] < n {
+			pre := stack.Pop()
+			res[pre] = i - pre
+		}
+
+		stack.Push(i)
+	}
+
+	return res
+}
