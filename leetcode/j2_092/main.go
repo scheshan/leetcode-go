52a4075172cacb4f8be6afa41534ae919b9a4fe9
scheshan <5373827@qq.com>
2023-01-12 15:36:53 +0800

    j2_092

---
+++ leetcode/j2_092/main.go
@@ -0,0 +1,31 @@
+package j2_092
+
+func minFlipsMonoIncr(s string) int {
+	n0 := 0
+	n1 := 0
+
+	for i, ch := range s {
+		if i == 0 {
+			if ch == '0' {
+				n1 = 1
+			} else {
+				n0 = 1
+			}
+		} else {
+			if ch == '0' {
+				n1 = min(n0, n1) + 1
+			} else {
+				n0, n1 = n0+1, min(n0, n1)
+			}
+		}
+	}
+
+	return min(n0, n1)
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
