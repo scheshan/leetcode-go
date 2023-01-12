e97fbadd6c205fd140f939ffe7a51baf7873a805
heshan <5373827@qq.com>
2023-01-12 22:18:40 +0800

    j_39

---
+++ leetcode/j_39/main.go
@@ -0,0 +1,19 @@
+package j_39
+
+func majorityElement(nums []int) int {
+	vote := 0
+	res := 0
+
+	for _, num := range nums {
+		if vote == 0 {
+			vote++
+			res = num
+		} else if num != res {
+			vote--
+		} else {
+			vote++
+		}
+	}
+
+	return res
+}
