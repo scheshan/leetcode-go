ca7f18d94fb60e4bacea68d3d77ae1e87d3a212f
scheshan <5373827@qq.com>
2023-01-12 17:50:27 +0800

    l677

---
+++ leetcode/l677/main.go
@@ -0,0 +1,62 @@
+package l677
+
+type MapSum struct {
+	root *Node
+}
+
+type Node struct {
+	count    int
+	children []*Node
+}
+
+/** Initialize your data structure here. */
+func Constructor() MapSum {
+	m := MapSum{
+		root: &Node{},
+	}
+	return m
+}
+
+func (this *MapSum) Insert(key string, val int) {
+	node := this.root
+	for ind, ch := range key {
+		if node.children == nil {
+			node.children = make([]*Node, 26)
+		}
+		child := node.children[ch-'a']
+		if child == nil {
+			child = &Node{}
+			node.children[ch-'a'] = child
+		}
+
+		if ind == len(key)-1 {
+			child.count = val
+		}
+		node = child
+	}
+}
+
+func (this *MapSum) Sum(prefix string) int {
+	node := this.root
+	for _, ch := range prefix {
+		if node.children == nil || node.children[ch-'a'] == nil {
+			return 0
+		}
+		node = node.children[ch-'a']
+	}
+	return this.dfs(node)
+}
+
+func (t *MapSum) dfs(node *Node) int {
+	if node == nil {
+		return 0
+	}
+
+	res := node.count
+	for _, child := range node.children {
+		if child != nil {
+			res += t.dfs(child)
+		}
+	}
+	return res
+}
