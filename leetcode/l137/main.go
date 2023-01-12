f733b1a8ee85a4d6f15415ebcfbadb1d20bfeaac
heshan <5373827@qq.com>
2023-01-12 21:29:29 +0800

    l137

---
+++ leetcode/l137/main.go
@@ -0,0 +1,24 @@
+package l137
+
+func singleNumber(nums []int) int {
+	arr := make([]int, 32)
+
+	for _, num := range nums {
+		n := int32(num)
+		for i := 0; i < 32; i++ {
+			if n&1 != 0 {
+				arr[i]++
+			}
+			n >>= 1
+		}
+	}
+
+	res := int32(0)
+	for i := 0; i < len(arr); i++ {
+		if arr[i]%3 > 0 {
+			res = res | (1 << i)
+		}
+	}
+
+	return int(res)
+}
