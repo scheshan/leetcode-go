9b5149cad7daab6b0a47e5850a311a5ce44d7321
heshan <5373827@qq.com>
2023-01-12 21:11:18 +0800

    l318

---
+++ leetcode/l318/main.go
@@ -0,0 +1,35 @@
+package l318
+
+func maxProduct(words []string) int {
+	m := make([][]int, 0)
+
+	res := 0
+	for _, word := range words {
+		bits := countBits(word)
+		if bits > 0 {
+			for _, exist := range m {
+				if bits&exist[0] == 0 {
+					res = max(res, len(word)*exist[1])
+				}
+			}
+			m = append(m, []int{bits, len(word)})
+		}
+	}
+	return res
+}
+
+func countBits(word string) int {
+	n := 0
+	for _, ch := range word {
+		s := ch - 'a'
+		n |= 1 << s
+	}
+	return n
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
