64bc6345eebd324ad6cc6d4e611341cbc3589d2f
scheshan <5373827@qq.com>
2023-01-13 16:24:01 +0800

    j2_022

---
+++ leetcode/j2_022/main.go
@@ -0,0 +1,31 @@
+package j2_022
+
+import "github.com/scheshan/leetcode/common"
+
+type ListNode = common.ListNode
+
+func detectCycle(head *ListNode) *ListNode {
+	slow := head
+	fast := head
+
+	for slow != nil && fast != nil {
+		if fast.Next == nil || fast.Next.Next == nil {
+			return nil
+		}
+
+		slow = slow.Next
+		fast = fast.Next.Next
+
+		if slow == fast {
+			break
+		}
+	}
+
+	slow = head
+	for slow != fast {
+		slow = slow.Next
+		fast = fast.Next
+	}
+
+	return slow
+}
