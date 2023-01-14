f6d81620d86002d42657234e2614caef0280f8b1
heshan <5373827@qq.com>
2023-01-14 17:38:24 +0800

    j_42

---
+++ offer/j_42/main.go
@@ -0,0 +1,25 @@
+package main
+
+func maxSubArray(nums []int) int {
+	min := 0
+	res := nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		nums[i] += nums[i-1]
+
+		if nums[i] > res {
+			res = nums[i]
+		}
+		if nums[i]-nums[min] > res {
+			res = nums[i] - nums[min]
+		}
+		if nums[i] < nums[min] {
+			min = i
+		}
+	}
+	return res
+}
+
+func main() {
+	maxSubArray([]int{-2, -1})
+}
