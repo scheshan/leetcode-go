7fa2d66b157c5efdb45f3360c52fd132c084df03
heshan <5373827@qq.com>
2023-01-15 18:14:23 +0800

    l437

---
+++ leetcode/l437/main.go
@@ -0,0 +1,31 @@
+package l437
+
+import "github.com/scheshan/leetcode/common"
+
+type TreeNode = common.TreeNode
+
+func pathSum(root *TreeNode, targetSum int) int {
+	m := make(map[int]int)
+	m[0] = 1
+	return dfs(root, targetSum, 0, m)
+}
+
+func dfs(node *TreeNode, target int, cur int, m map[int]int) int {
+	if node == nil {
+		return 0
+	}
+
+	res := 0
+	cur += node.Val
+	delta := cur - target
+	res += m[delta]
+
+	m[cur]++
+
+	res += dfs(node.Left, target, cur, m)
+	res += dfs(node.Right, target, cur, m)
+
+	m[cur]--
+
+	return res
+}
