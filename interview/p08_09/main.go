198f96ff65ea58ae4310a00b6e78533306cef01f
scheshan <5373827@qq.com>
2023-02-07 15:47:16 +0800

    p08_09

---
+++ interview/p08_09/main.go
@@ -0,0 +1,26 @@
+package p08_09
+
+func generateParenthesis(n int) []string {
+	res := make([]string, 0)
+	path := make([]byte, 0, n<<1)
+	backtrack(n, 0, 0, &path, &res)
+	return res
+}
+
+func backtrack(n int, left int, right int, path *[]byte, res *[]string) {
+	if len(*path) == n<<1 {
+		*res = append(*res, string(*path))
+		return
+	}
+
+	if left < n {
+		*path = append(*path, '(')
+		backtrack(n, left+1, right, path, res)
+		*path = (*path)[:len(*path)-1]
+	}
+	if right < left {
+		*path = append(*path, ')')
+		backtrack(n, left, right+1, path, res)
+		*path = (*path)[:len(*path)-1]
+	}
+}
