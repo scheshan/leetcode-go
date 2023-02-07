b6f8fc5b6e9fd9fa8b545705ce13c8819632683c
scheshan <5373827@qq.com>
2023-02-07 15:40:32 +0800

    p1038

---
+++ leetcode/p1038/main.go
@@ -0,0 +1,21 @@
+package p1038
+
+import "github.com/scheshan/leetcode/common"
+
+type TreeNode = common.TreeNode
+
+func bstToGst(root *TreeNode) *TreeNode {
+	total := 0
+	var dfs func(node *TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		dfs(node.Right)
+		node.Val += total
+		total = node.Val
+		dfs(node.Left)
+	}
+	dfs(root)
+	return root
+}
