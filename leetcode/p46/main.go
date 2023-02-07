a22936dd5e8bb5df6ab8e0a4b288fe5d4f23953f
scheshan <5373827@qq.com>
2023-02-07 15:17:53 +0800

    p46

---
+++ leetcode/p46/main.go
@@ -0,0 +1,23 @@
+package p46
+
+func permute(nums []int) [][]int {
+	res := make([][]int, 0)
+
+	backtrack(nums, 0, &res)
+	return res
+}
+
+func backtrack(nums []int, ind int, res *[][]int) {
+	if ind == len(nums) {
+		arr := make([]int, len(nums))
+		copy(arr, nums)
+		*res = append(*res, arr)
+		return
+	}
+
+	for i := ind; i < len(nums); i++ {
+		nums[ind], nums[i] = nums[i], nums[ind]
+		backtrack(nums, ind+1, res)
+		nums[ind], nums[i] = nums[i], nums[ind]
+	}
+}
