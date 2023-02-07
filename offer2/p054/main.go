8c2c0c6ad8a4e854232913bf226d795b6994dae1
scheshan <5373827@qq.com>
2023-02-07 15:38:14 +0800

    offer2 p054

---
+++ offer2/p054/main.go
@@ -0,0 +1,23 @@
+package p054
+
+import (
+	"github.com/scheshan/leetcode/common"
+)
+
+type TreeNode = common.TreeNode
+
+func convertBST(root *TreeNode) *TreeNode {
+	total := 0
+	var dfs func(node *TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		dfs(node.Right)
+		node.Val += total
+		total = node.Val
+		dfs(node.Left)
+	}
+	dfs(root)
+	return root
+}
