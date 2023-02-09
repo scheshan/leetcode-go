22bb673eac23cca9bd299bd1309c0a4bd644c9ba
scheshan <5373827@qq.com>
2023-02-09 15:03:37 +0800

    p025

---
+++ offer2/p025/main.go
@@ -0,0 +1,47 @@
+package p025
+
+import "github.com/scheshan/leetcode/common"
+
+type ListNode = common.ListNode
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	l1, l2 = reverse(l1), reverse(l2)
+
+	dummy := &ListNode{}
+	tail := dummy
+	promotion := false
+
+	for l1 != nil || l2 != nil || promotion {
+		n := 0
+		if promotion {
+			n = 1
+			promotion = false
+		}
+		if l1 != nil {
+			n += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			n += l2.Val
+			l2 = l2.Next
+		}
+		if n >= 10 {
+			n -= 10
+			promotion = true
+		}
+		tail.Next = &ListNode{Val: n}
+		tail = tail.Next
+	}
+	return reverse(dummy.Next)
+}
+
+func reverse(head *ListNode) *ListNode {
+	var res *ListNode
+	for head != nil {
+		next := head.Next
+		head.Next = res
+		res = head
+		head = next
+	}
+	return res
+}
