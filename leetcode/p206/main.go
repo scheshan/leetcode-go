45dbaacebfa05447f6365f30181c67149cc1db1d
scheshan <5373827@qq.com>
2023-02-09 14:59:01 +0800

    p206

---
+++ leetcode/p206/main.go
@@ -0,0 +1,19 @@
+package p206
+
+import "github.com/scheshan/leetcode/common"
+
+type ListNode = common.ListNode
+
+func reverseList(head *ListNode) *ListNode {
+	var res *ListNode
+
+	for head != nil {
+		next := head.Next
+		head.Next = res
+		res = head
+
+		head = next
+	}
+
+	return res
+}
