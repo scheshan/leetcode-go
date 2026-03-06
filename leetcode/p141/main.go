3bccaf7194dd8eeec3299ebed7d684c452615ddc
scheshan <5373827@qq.com>
2026-03-06 08:53:15 +0800

    leetcode p141

---
+++ leetcode/p141/main.go
@@ -0,0 +1,25 @@
+package p141
+
+import "github.com/scheshan/leetcode/common"
+
+type ListNode = common.ListNode
+
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+
+	slow := head
+	fast := head.Next
+
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+
+		if slow == fast {
+			return true
+		}
+	}
+
+	return false
+}
